f18837a0864f8d3ab74ecc3699bf363f39619588
Faissal Lauar <faissal.lauar@aurea.com>
2023-05-24 17:32:47 -0300

    using ServiceLoader

---
+++ src/org.fastsocket/org/fastsocket/FastNetworkSocket.java
@@ -0,0 +1,11 @@
+package org.fastsocket;
+
+import java.io.IOException;
+
+import com.socket.NetworkSocket;
+
+public class FastNetworkSocket extends NetworkSocket {
+    FastNetworkSocket() { }
+    @Override
+    public void close() throws IOException { }
+}
